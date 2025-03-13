f5a34027f0a79ad56d3cd1b9b70d7535eb5df94b
Johan Octavio Morales <johanmobe18@gmail.com>
2025-03-13 13:35:58 -0600

    Ahora se puede preguntar por más perros.

--- Perros.py
+++ Perros.py
@@ -1,7 +1,13 @@
-nombre = input("¿Cuál es el nombre de tu perro? ")
-edad_perro = int(input(f"¿Cuántos años tiene {nombre}? "))
+while True:
+    nombre = input("¿Cuál es el nombre de tu perro? ")
+    edad_perro = int(input(f"¿Cuántos años tiene {nombre}? "))
 
-edad_humana = edad_perro * 7
+    edad_humana = edad_perro * 7
 
-print(f"{nombre} tiene {edad_perro} años, lo que equivale a {edad_humana} años humanos.")
+    print(f"{nombre} tiene {edad_perro} años, lo que equivale a {edad_humana} años humanos.")
+    
+    agregar_otro = input("¿Quieres agregar otro perro? (sí/no): ").lower()
+    if agregar_otro != 'sí':
+        break
+
 input("Presiona Enter para salir...")
